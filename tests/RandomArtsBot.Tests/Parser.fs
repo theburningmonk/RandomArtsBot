ebf7b2138682ac5ff826649d38d336bb36ca78da
Yan Cui <yan.cui@just-eat.com>
2016-01-30 03:18:26 +0000

    - added more tests

--- tests/RandomArtsBot.Tests/Parser.fs
+++ tests/RandomArtsBot.Tests/Parser.fs
@@ -2,6 +2,8 @@
 
 open FsUnit
 open NUnit.Framework
+open FsCheck
+
 open RandomArtsBot.RandomArt
 
 module Option =
@@ -11,57 +13,12 @@ module Option =
 
 module ``Parser tests`` =
     [<Test>]
-    let ``"x", "y" and "const" should be parsed as simple expressions`` () =
-        parse "x" |> Option.ofChoice |> should equal <| Some VariableX
-        parse "y" |> Option.ofChoice |> should equal <| Some VariableY
-        parse "const" |> Option.ofChoice |> should equal <| Some Constant
-        
-    [<Test>]
-    let ``"(sin x)" should be parsed as a Sin expression`` () =
-        parse "(sin x)"
-        |> Option.ofChoice
-        |> should equal (Some <| Sin VariableX)
-        
-    [<Test>]
-    let ``"(cos x)" should be parsed as a Cos expression`` () =
-        parse "(cos x)"
-        |> Option.ofChoice
-        |> should equal (Some <| Cos VariableX)
-        
-    [<Test>]
-    let ``"(tan x)" should be parsed as a Tan expression`` () =
-        parse "(tan x)"
-        |> Option.ofChoice
-        |> should equal (Some <| Tan VariableX)
-        
-    [<Test>]
-    let ``"(+ (tan x) (cos y))" should be parsed as a nested Add expression`` () =
-        let expected = Add ((Tan VariableX), (Cos VariableY))
+    let ``expressions generated from Expr should be parsed to the same Expr`` () = 
+        let property (expr : Expr) =
+            let expr' = 
+                expr.ToString() 
+                |> parse 
+                |> Option.ofChoice
+            expr' = Some expr
 
-        parse "(+ (tan x) (cos y))"
-        |> Option.ofChoice
-        |> should equal (Some expected)
-        
-    [<Test>]
-    let ``"(- (Sqr x) const)" should be parsed as a nested Subtract expression`` () =
-        let expected = Subtract ((Sqr VariableX), Constant)
-
-        parse "(- (Sqr x) const)"
-        |> Option.ofChoice
-        |> should equal (Some expected)
-        
-    [<Test>]
-    let ``"(mix (Sqr x) const (* (Avg x y) (Sqrt const)))" should be parsed as a nested Mix expression`` () =
-        let expected = 
-            Mix (
-                Sqr VariableX, 
-                Constant,
-                Product (
-                    Average (VariableX, VariableY),
-                    Sqrt Constant
-                )
-            )
-
-        parse "(mix (Sqr x) const (* (Avg x y) (Sqrt const)))"
-        |> Option.ofChoice
-        |> should equal (Some expected)
+        Check.VerboseThrowOnFailure property
